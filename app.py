24e50debcc3c2f7cf140fa493cdaff46a9d3dd62
pri0r <51406858+pri0r@users.noreply.github.com>
2021-04-24 02:29:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -295,34 +295,22 @@ def main():
         st.map(df[df['lat'].notnull()][['lat','lon']])
               
 
-        st.pydeck_chart(pdk.Deck(
-        map_style='mapbox://styles/mapbox/light-v9',
-        initial_view_state=pdk.ViewState(
-            latitude=37.76,
-            longitude=-122.4,
-            zoom=11,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-               'HexagonLayer',
-               data=df,
-               get_position='[lon, lat]',
-               radius=200,
-               elevation_scale=4,
-               elevation_range=[0, 1000],
-               pickable=True,
-               extruded=True,
-            ),
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=df,
-                get_position='[lon, lat]',
-                get_color='[200, 30, 0, 160]',
-                get_radius=200,
-            ),
-        ],
-        ))      
+
+
+        counties = alt.topo_feature(df, 'Country_Region')
+        source = df#[df['Confirmed']]
+
+        st.alt.Chart(counties).mark_geoshape().encode(
+            color='Deaths'
+        ).transform_lookup(
+            lookup='Admin2',
+            from_=alt.LookupData(source, 'Admin2', ['Recovered'])
+        ).project(
+            type='albersUsa'
+        ).properties(
+            width=500,
+            height=300
+        )   
               
               
               
