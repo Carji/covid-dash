d349cda175d9679b671e5fc352e796c9707d387d
Carji <51406858+Carji@users.noreply.github.com>
2022-12-30 17:01:34 +0100

    updated data with full 2022

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ def main():
         df=confirmedDf      
 ## MAP
 
-        st.markdown("Mapas descriptivos de la situación global del Covid-19 (actualizado a 2021-05-22 04:20:45)")
+        st.markdown("Mapas descriptivos de la situación global del Covid-19 (actualizado a 2022-12-29 16:59:41)")
 
         st.map(df[df['lat'].notnull()][['lat','lon']])
               
